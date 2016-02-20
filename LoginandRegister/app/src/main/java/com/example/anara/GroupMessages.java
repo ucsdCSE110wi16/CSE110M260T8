a0c0bd0758e7702fe4a4ca8dec880855f00a4e2c
thearad <anaradiccc@gmail.com>
2016-02-20 08:39:21 -0800

    Classes

---
+++ LoginandRegister/app/src/main/java/com/example/anara/GroupMessages.java
@@ -0,0 +1,17 @@
+package com.example.anara.myapplication;
+
+/**
+ * Created by anara on 2/18/2016.
+ */
+public class GroupMessages {
+    Integer messageId;
+    Integer groupId;
+
+    public void setGMessageId( Integer messageId ){ this.messageId = messageId; }
+
+    public Integer getGMessageId(){ return messageId; }
+
+    public void setGGroupId( Integer groupId ){ this.groupId = groupId; }
+
+    public Integer getGGroupId(){ return groupId; }
+}
