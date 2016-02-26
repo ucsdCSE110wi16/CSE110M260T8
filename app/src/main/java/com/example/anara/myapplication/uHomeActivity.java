2fe4078bdc1136c7ab75db88b082f7a1e20ee1b1
thearad <anaradiccc@gmail.com>
2016-02-26 09:25:12 -0800

    Added some layout for groups list

--- app/src/main/java/com/example/anara/myapplication/uHomeActivity.java
+++ app/src/main/java/com/example/anara/myapplication/uHomeActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.view.View;
@@ -14,71 +19,65 @@ import com.firebase.ui.FirebaseListAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class uHomeActivity extends ListActivity {
-    Firebase ref = new Firebase("https://burning-fire-7007.firebaseio.com/groupMembers");
-    Query queryRef = ref.orderByChild("userId").equalTo(ref.getAuth().getUid());
+public class uHomeActivity extends AppCompatActivity {
+
+    protected ArrayList<String> groupNames = new ArrayList<>();
+    Firebase ref = new Firebase("https://burning-fire-7007.firebaseio.com/");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_home);
-        ListView groupView = (ListView) findViewById(android.R.id.list);
+        final ListView groupView = (ListView) findViewById(android.R.id.list);
         Firebase.setAndroidContext(this);
 
-        FirebaseListAdapter<Group> mAdapter = new FirebaseListAdapter<Group>(this, Group.class, android.R.layout.simple_list_item_1, queryRef) {
-            @Override
-            protected void populateView(android.view.View view, Group group, int position) {
-                ((TextView)view.findViewById(R.id.groupName)).setText(group.getGroupName());
-            }
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup) inflater.inflate(R.layout.header, groupView, false);
+        groupView.addHeaderView(header, null, false);
 
-        };
-        /*queryRef.addChildEventListener(new ChildEventListener() {
+        Firebase mRef = ref.child("groupMembers");
+        Query queryRef = mRef.orderByChild("userMem").equalTo(ref.getAuth().getUid());
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
-                HashMap<String, Object> groups = (HashMap<String, Object>) snapshot.getValue();
-                for (Object group : groups.values()) {
-                    HashMap<String, Object> groupMap = (HashMap<String, Object>) group;
-                    String groupId = (String) groupMap.remove("id");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                groupNames.clear();
+                for(DataSnapshot child: dataSnapshot.getChildren()){
+                    String groupId = (String) child.child("groupMem").getValue();
                     Firebase gRef = new Firebase("https://burning-fire-7007.firebaseio.com/group");
-                    Query querygRef = gRef.orderByChild("name");
-                    querygRef.addChildEventListener(new ChildEventListener(){
+                    Query qGref = gRef.orderByChild("groupId").equalTo(groupId);
+                    /*FirebaseListAdapter<Group> mAdapter = new FirebaseListAdapter<Group>(this, Group.class, android.R.layout.simple_list_item_1, qGref) {
                         @Override
-                        public void onChildAdded(DataSnapshot snapshot, String previousChild) {
-                            HashMap<String, Object> groups = (HashMap<String, Object>) snapshot.getValue();
-                        for(Object group : groups.values()) {
-                            HashMap<String, Object> groupMap = (HashMap<String, Object>) group;
-                            String groupId = (String) groupMap.remove("id");
-                            if(!list.contains(groupId)) {
-                                String name = (String) groupMap.remove("name");
-                                String description = (String) groupMap.remove("desc");
-                                Group result = new Group(groupId, name, description);
-                                list.add(result);
+                        protected void populateView(android.view.View view, Group group, int position) {
+                            ((TextView)view.findViewById(R.id.groupName)).setText(group.getGroupName());
+                        }
+
+                    };*/
+                    qGref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for(DataSnapshot child: dataSnapshot.getChildren()) {
+                                String name = (String) child.child("groupName").getValue();
+                                groupNames.add(name);
+                                Log.v("uHomeActivity","groups = " + groupNames);
                             }
                         }
-                    }
-                        public void onChildRemoved(DataSnapshot snapshot){}
-                        public void onChildChanged(DataSnapshot snapshot, String string){}
-                        public void onChildMoved(DataSnapshot snapshot, String string){}
-                        public void onCancelled(FirebaseError e){}
+
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+
+                        }
                     });
-                    // Query for all people contacts using the Contacts.People convenience class.
-                    // Put a managed wrapper around the retrieved cursor so we don't have to worry about
-                    // requerying or closing it as the activity changes state.
-                    mCursor = this.getContentResolver().query(People.CONTENT_URI, null, null, null, null);
-                    startManagingCursor(mCursor);
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(uHomeActivity.this, android.R.layout.simple_list_item_multiple_choice,groupNames);
 
-                    // Now create a new list adapter bound to the cursor.
-                    // SimpleListAdapter is designed for binding to a Cursor.
-                    ListAdapter adapter = new SimpleCursorAdapter(
-                            this, // Context.
-                            android.R.layout.two_line_list_item,  // Specify the row template to use (here, two columns bound to the two retrieved cursor
-                            rows).
-                            mCursor,                                              // Pass in the cursor to bind to.
-                    new String[]{People.NAME, People.COMPANY},           // Array of cursor columns to bind to.
-                            new int[]{android.R.id.text1, android.R.id.text2})*/
-        ;  // Parallel array of which template objects to bind to those columns.
+                groupView.setAdapter(adapter);
+            }
 
-                    // Bind to our new adapter.
-                groupView.setAdapter(mAdapter);
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e("uHomeActivity", firebaseError.getMessage());
+            }
+        });
     }
 
     public void searchG(android.view.View button){
