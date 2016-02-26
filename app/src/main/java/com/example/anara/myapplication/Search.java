36f81a877eb2f814a85581f614a124343da4ce3c
So mang(Joseph) Jung <josephjung426@gmail.com>
2016-02-26 01:00:44 -0800

    Finished Search

--- app/src/main/java/com/example/anara/myapplication/Search.java
+++ app/src/main/java/com/example/anara/myapplication/Search.java
@@ -4,18 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.GenericTypeIndicator;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import android.view.View.OnKeyListener;
+
 
 /**
  * Created by Joseph on 2/24/2016.
@@ -27,10 +37,9 @@ public class Search extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     ListView listView;
     EditText editText;
+    int checkDel = 0;
+    String finalData;
 
-
-    Firebase database = new Firebase("https://burning-fire-7007.firebaseio.com/user");
-    Query queryRef = database.orderByChild("groupId");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +62,11 @@ public class Search extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkDel++;
 
-
-                if (s.toString().equals("")) {
+                if (s.toString().equals("") || checkDel == count + 2) {
                     initList();
-                } else {
-                    searchItem(s.toString());
+                    checkDel = count;
                 }
             }
 
@@ -68,26 +76,97 @@ public class Search extends AppCompatActivity {
             }
         });
 
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                    searchItem(editText.getText().toString());
+                    return true;
+                }
+
+
+                return false;
+            }
+        });
+
+
+
     }
 
     private void searchItem(String textToSearch) {
-        for(String item:items){
-            if(!item.toLowerCase().contains(textToSearch.toLowerCase())) {
-                listItems.remove(item);
+        for(int i = listItems.size()-1; i>= 0; i--) {
+            if (!listItems.get(i).toLowerCase().contains(textToSearch.toLowerCase())) {
+                listItems.remove(i);
+                adapter.notifyDataSetChanged();
             }
 
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(Search.this, "Will go into "+listItems.get(position), Toast.LENGTH_SHORT).show();;
+                    finalData = "" + listItems.get(position);
+
+
+                }
+            });
         }
+
+        if(listItems.size() == 0)
+        {
+            Toast.makeText(Search.this, "No Group With Such Name", Toast.LENGTH_SHORT).show();
+        }
+
 
         adapter.notifyDataSetChanged();
     }
 
-    public void initList(){
+    public String clickedGroup(){
+        return finalData;
+    }
+
+    private void initList(){
+        Firebase database = new Firebase("https://burning-fire-7007.firebaseio.com/group");
+        Query query = database.orderByValue();
+        listItems = new ArrayList<String>();
+
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
 
 
-        //items = new String[]{database.toString()};
-        listItems= new ArrayList<>(Arrays.asList(database.toString()));
+                String nameofgroup = dataSnapshot.getValue().toString();
+
+                String[] datainstring = nameofgroup.split(", ");
+                ArrayList<String> groupname = new ArrayList<String>();
+                for (int i = 0; i < datainstring.length; i++) {
+                    if (datainstring[i].contains("groupName")) {
+                        groupname.add(datainstring[i]);
+                    }
+                }
+                for (int i = 0; i < groupname.size(); i++) {
+                    String old = groupname.get(i);
+                    String justname = old.substring(10);
+                    groupname.set(i, justname);
+
+                    listItems.add(groupname.get(i));
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+
+
         adapter = new ArrayAdapter<String>(this, R.layout.listitem_layout,R.id.txtitem,listItems);
         listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
