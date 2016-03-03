3806308ae6a9aa8a84e81a4f9d08b052fd334261
xiejiashan <xiejiashan123@gmail.com>
2016-03-02 20:58:46 -0800

    Group Pg final version

--- app/src/main/java/com/example/anara/myapplication/CreatGroupActivity.java
+++ app/src/main/java/com/example/anara/myapplication/CreatGroupActivity.java
@@ -1,17 +1,15 @@
 package com.example.anara.myapplication;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
@@ -20,7 +18,6 @@ import java.util.ArrayList;
 public class CreatGroupActivity extends AppCompatActivity {
 
    Firebase myRef = new Firebase("https://burning-fire-7007.firebaseio.com");
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +31,11 @@ public class CreatGroupActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -50,7 +47,8 @@ public class CreatGroupActivity extends AppCompatActivity {
         String description = etDesc.getText().toString();
 
         if(name == "" || description == ""){
-         //   name = "";
+            Toast toast = Toast.makeText(CreatGroupActivity.this, "Can't be empty!", Toast.LENGTH_SHORT);
+            toast.show();
             finish();
         }
         else {
--- app/src/main/java/com/example/anara/myapplication/GroupActivity.java
+++ app/src/main/java/com/example/anara/myapplication/GroupActivity.java
@@ -1,39 +1,29 @@
 package com.example.anara.myapplication;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
-
 import java.util.ArrayList;
-import java.util.Hashtable;
 
 public class GroupActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button joinButton;
     Button chatButton;
     TextView descTextView;
-    ListView listView;
     ArrayList<String> groupid = new ArrayList<>();//to get the groupid from prev activity
     ArrayList<String> members = new ArrayList<>();
     Firebase myRef = new Firebase("https://burning-fire-7007.firebaseio.com");
@@ -55,12 +45,7 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
         Intent curr = this.getIntent();
         if (curr.getStringArrayListExtra("groupid") != null)
             groupid = curr.getStringArrayListExtra("groupid");
-        System.out.println(groupid);
-
-        /*
-        if (curr.getStringArrayListExtra("userid") != null)
-            userid = curr.getStringArrayListExtra("userid");
-        else userid.add("no user id passed from creatgroup");*/
+  //      System.out.println(groupid);
 
         final Firebase myFirebaseRef = myRef.child("group");
         Query queryRef = myFirebaseRef.orderByChild("groupId").equalTo(groupid.get(0));
@@ -77,7 +62,6 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-                //       Log.e("uHomeActivity", firebaseError.getMessage());
             }
         });
 
@@ -89,23 +73,22 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
         userView.addHeaderView(header, null, false);
         Firebase mRef = myRef.child("groupMembers");
         Query userqueryRef = mRef.orderByChild("groupMem").equalTo(groupid.get(0));
-        System.out.println(groupid.get(0));
+    //    System.out.println(groupid.get(0));
         userqueryRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 members.clear();
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
                     String userId = (String) child.child("userMem").getValue();//UID
-                    System.out.println(userId);
+               //     System.out.println(userId);
                     Firebase gRef = myRef.child("user");
-                    //= new Firebase("https://burning-fire-7007.firebaseio.com/user");
                     Query qGref = gRef.orderByChild("userId").equalTo(userId);
                     qGref.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             for (DataSnapshot child : dataSnapshot.getChildren()) {
                                 String email = (String) child.child("email").getValue();
-                                System.out.println(email);
+                          //      System.out.println(email);
                                 System.out.println(myRef.getAuth().getProviderData().get("email"));
                                 if (email == myRef.getAuth().getProviderData().get("email"))
                                     inGroup = true;
@@ -148,7 +131,7 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
                 }
                 else
                 {
-                    Toast toast = Toast.makeText(GroupActivity.this, "Welcome to group "+ grpName +"!", Toast.LENGTH_SHORT);
+                    Toast toast = Toast.makeText(GroupActivity.this, "Welcome to "+ grpName +"!", Toast.LENGTH_SHORT);
                     toast.show();
                     GroupMembers gm = new GroupMembers();
                     gm.setGroupMem(groupid.get(0));
@@ -159,6 +142,5 @@ public class GroupActivity extends AppCompatActivity implements View.OnClickList
                 }
                 break;
         }
-           //     finish();
         }
     }
