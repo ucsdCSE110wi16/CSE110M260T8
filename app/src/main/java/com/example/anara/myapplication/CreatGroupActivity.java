119be6854873239745ddd239fcf27d9230dea5cf
So mang(Joseph) Jung <josephjung426@gmail.com>
2016-03-03 16:23:56 -0800

    Got rid of floating button

--- app/src/main/java/com/example/anara/myapplication/CreatGroupActivity.java
+++ app/src/main/java/com/example/anara/myapplication/CreatGroupActivity.java
@@ -23,14 +23,7 @@ public class CreatGroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_group);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
 
     }
 
